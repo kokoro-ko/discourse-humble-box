faf389af2b115023db2712643d19d3fd4867e64d
Machigatta <iappware@googlemail.com>
2017-10-15 00:39:52 +0200

    major logic change

--- assets/javascripts/humble_box.js
+++ assets/javascripts/humble_box.js
@@ -2,22 +2,25 @@
 
     var DEFAULTS = {
         viewUrl: "https://api.kokoro-ko.de/humblebox/index.php",
-        reg: new RegExp(/^https?:\/\/(?:www\.)?humblebundle\.com\/([a-zA-Z0-9-_]{1,25})\/?([a-zA-Z0-9-_]{1,25})?/)
+        reg: new RegExp(/^https?:\/\/(?:www\.)?humblebundle\.com\/([a-zA-Z0-9_-]{1,45})\/?([a-zA-Z0-9_-]{1,45})?/)
     };
 
     function applyBox($el, $options) {
         var href = $($el).attr("href");
+
         if (DEFAULTS.reg.test(href)) {
+
             var excP = DEFAULTS.reg.exec(href);
             if (excP == null) {
                 return;
             }
 
             var type, objCode = "";
-            if (excP.length > 1 && (excP[1] == "books" || excP[1] == "software" || excP[1] == "store")) {
+
+            if (excP[2] !== undefined > 2 && (excP[1] == "books" || excP[1] == "software" || excP[1] == "store")) {
                 type = excP[1];
                 objCode = excP[2];
-            } else if (excP.length == 1) {
+            } else if (excP[2] == undefined) {
                 type = "games";
                 objCode = excP[1];
             } else {
@@ -27,11 +30,9 @@
             if (type == "books" || type == "software" || type == "store") {
                 //TO-DO
             } else {
+
                 $($el).closest(".onebox").replaceWith('<iframe src="' + DEFAULTS.viewUrl + '?type=' + type + '&urlCode=' + objCode + '" frameborder="0" width="100%" height="150"></iframe>');
             }
-
-
-
         }
     }
 
